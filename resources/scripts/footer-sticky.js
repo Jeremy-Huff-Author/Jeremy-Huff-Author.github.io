f5de26a2b103192b8209610808d8259e28b66c2e
jeremythuff <jeremythuff@gmail.com>
2025-05-24 05:15:26 +0000

    subtract footer height

--- resources/scripts/footer-sticky.js
+++ resources/scripts/footer-sticky.js
@@ -2,7 +2,7 @@ const checkFooterSticky = () => {
   const mainContent = document.querySelector('main');
   const viewportHeight = window.innerHeight;
 
-  if (mainContent.offsetHeight <= viewportHeight) {
+  if (mainContent.offsetHeight <= viewportHeight-75) {
     document.body.classList.add('sticky-footer');
   } else {
     document.body.classList.remove('sticky-footer');
