56760e4cff053c215946a64209e47e9b6995d534
jeremythuff <jeremythuff@gmail.com>
2025-05-24 05:17:47 +0000

    using scroll height

--- resources/scripts/footer-sticky.js
+++ resources/scripts/footer-sticky.js
@@ -1,8 +1,8 @@
 const checkFooterSticky = () => {
-  const mainContent = document.querySelector('main');
+  const contentHeight = document.documentElement.scrollHeight;
   const viewportHeight = window.innerHeight;
 
-  if (mainContent.offsetHeight <= viewportHeight-75) {
+  if (contentHeight <= viewportHeight) {
     document.body.classList.add('sticky-footer');
   } else {
     document.body.classList.remove('sticky-footer');
