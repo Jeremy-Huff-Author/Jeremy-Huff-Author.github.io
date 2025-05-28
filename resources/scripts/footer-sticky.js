8ffae084b001e433f91b5eb650f2813f3de08520
jeremythuff <jeremythuff@gmail.com>
2025-05-28 20:20:23 +0000

    Accounting for footer height in sticky footer calculation

--- resources/scripts/footer-sticky.js
+++ resources/scripts/footer-sticky.js
@@ -1,8 +1,10 @@
 const checkFooterSticky = () => {
   const contentHeight = document.documentElement.scrollHeight;
   const viewportHeight = window.innerHeight;
+  const footer = document.querySelector('footer');
+  const footerHeight = footer ? footer.offsetHeight : 0;
 
-  if (contentHeight <= viewportHeight) {
+  if (contentHeight <= viewportHeight - footerHeight) {
     document.body.classList.add('sticky-footer');
   } else {
     document.body.classList.remove('sticky-footer');
