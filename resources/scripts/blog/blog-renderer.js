daa70791166ad51fb14ded61209956232bd88363
jeremythuff <jeremythuff@gmail.com>
2025-05-31 20:27:42 +0000

    Registering page load by post

--- resources/scripts/blog/blog-renderer.js
+++ resources/scripts/blog/blog-renderer.js
@@ -59,8 +59,10 @@ const renderPost = (postName) => {
     heroSection.appendChild(textOverlay);
     postContentContainer.appendChild(heroSection);
 
-    const htmlContent = window.marked.parse(markdownContent);
-    postContentContainer.innerHTML += htmlContent; // Add the body content
+    gtag('event', 'page_view', {
+      page_path: `/blog/index.html?post=${initialPostName}`,
+      page_title: initialPostName.replace(/-/g, ' ') // optional
+    });
     
     // Fetch and apply custom styles
     const customStylesPath = `${postFolderPath}/custom-styles.css`;
