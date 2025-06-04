45bada3bfe3876dffeb32ca1fa2f160ee601bab6
jeremythuff <jeremythuff@gmail.com>
2025-06-04 18:19:14 +0000

    added logging

--- resources/scripts/footer-sticky.js
+++ resources/scripts/footer-sticky.js
@@ -4,6 +4,11 @@ const checkFooterSticky = () => {
   const footer = document.querySelector('footer');
   const footerHeight = footer ? footer.offsetHeight : 0;
 
+  console.log("contentHeight", contentHeight);
+  console.log("viewportHeight", viewportHeight);
+  console.log("footerHeight", footerHeight);
+  console.log("contentHeight <= viewportHeight - footerHeight", contentHeight <= viewportHeight - footerHeight);
+
   if (contentHeight <= viewportHeight - footerHeight) {
     document.body.classList.add('sticky-footer');
   } else {
