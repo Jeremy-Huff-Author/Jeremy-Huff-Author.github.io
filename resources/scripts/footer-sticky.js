ed543a3b1061ae2d601fa6c1727d4e1c7ec6b535
Jeremy Huff <jeremythuff@gmail.com>
2025-06-04 13:09:28 -0500

    Fix sticky footer logic

--- resources/scripts/footer-sticky.js
+++ resources/scripts/footer-sticky.js
@@ -1,10 +1,8 @@
 const checkFooterSticky = () => {
   const contentHeight = document.documentElement.scrollHeight;
   const viewportHeight = window.innerHeight;
-  const footer = document.querySelector('footer');
-  const footerHeight = footer ? footer.offsetHeight : 0;
-
-  if (contentHeight <= viewportHeight - footerHeight) {
+  
+  if (contentHeight < viewportHeight) {
     document.body.classList.add('sticky-footer');
   } else {
     document.body.classList.remove('sticky-footer');
