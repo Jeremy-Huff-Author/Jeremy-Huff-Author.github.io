0b81c2a2ffead609869004de5ba736c4fc2646d9
jeremythuff <jeremythuff@gmail.com>
2025-06-04 01:47:10 +0000

    added logging

--- resources/scripts/navigation.js
+++ resources/scripts/navigation.js
@@ -60,7 +60,10 @@ if(navbarTogler) {
 const updateNavbarOpacity = () => {
 
     const navbarCollape = document.querySelector('.navbar-collapse');
-    if (navbarCollape && !(navbarCollape.classList.contains('show') || navbarCollape.classList.contains('collapsing'))) {
+    console.log("navbarCollape.classList.contains('show')", navbarCollape.classList.contains('show'));
+    console.log("navbarCollape.classList.contains('collapsing')", navbarCollape.classList.contains('collapsing'));
+    console.log("navbarCollape && !(navbarCollape.classList.contains('show') || navbarCollape.classList.contains('collapsing'))");
+    if (navbarCollape && !(navbarCollape.classList.contains('show') || navbarCollape.classList.contains('collapsing')))
         return
     }
 
